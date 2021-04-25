3c43460a1097698e710d2d7a3902662d018104c3
Phodal Huang <h@phodal.com>
2021-04-25 16:24:19 +0800

    docs: update content

--- samples/trace.py
+++ samples/trace.py
@@ -1,15 +1,28 @@
+import threading
+import time
 import tracemalloc
 
+def mwpoller():
+  while 1:
+    time.sleep(0.2)
+    mwpoll()
+
+def mwpoll():
+    snapshot = tracemalloc.take_snapshot()
+
+    for stat in snapshot.statistics("lineno"):
+        if str(stat.traceback).__contains__("trace.py"):
+            print(stat)
+
 tracemalloc.start()
+threading.Thread(target=mwpoller).start()
 
 l1 = [i for i in range(10000)]
 l2 = [i*i for i in range(10000)]
 l3 = [i*i*i for i in range(10000)]
+#
+# for trace in snapshot.traces:
+#     print(trace)
 
-snapshot = tracemalloc.take_snapshot()
-
-for stat in snapshot.statistics("lineno"):
-    print(stat)
-
-for trace in snapshot.traces:
-    print(trace)
+# tracemalloc.stop()
+exit()
