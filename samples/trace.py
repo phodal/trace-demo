5d5de2192bf9f61022e823c275a55e364a73c97b
Phodal Huang <h@phodal.com>
2021-04-25 15:35:13 +0800

    docs: try python version

---
+++ samples/trace.py
@@ -0,0 +1,12 @@
+import tracemalloc
+
+tracemalloc.start()
+
+l1 = [i for i in range(10000)]
+l2 = [i*i for i in range(10000)]
+l3 = [i*i*i for i in range(10000)]
+
+snapshot = tracemalloc.take_snapshot()
+
+for stat in snapshot.statistics("lineno"):
+    print(stat)
